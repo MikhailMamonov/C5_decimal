bd0424765daa43a2a02e5e0b061b03683254ee07
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-06-22 16:37:06 +0300

    [brittden:develop-1] - Initial commit: added header file, initialized tests.

---
+++ src/tests/test_s21_add.c
@@ -0,0 +1,81 @@
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../s21_decimal.h"
+#include "test_s21_common.h"
+
+static int compare_decimal(const s21_decimal *a, const s21_decimal *b){
+    for (int i=0;i<COUNT_OF_BITS;i++){
+        if(a->bits[i]!=b->bits[i]){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+static void run_add_test(addParams *params) {
+  s21_decimal result = {{0}};/*  */
+
+  int return_code = s21_add(params->value1, params->value2, &result);
+
+  ck_assert_int_eq(return_code, params->expected_return_code);
+
+  if(return_code==SUCCESS){
+    ck_assert_msg(compare_decimal(&result, &params->expected_result),
+        "test '%s' failed: result not match with expected.",
+        params->test_name);
+    ck_assert_ptr_ne((void *)&result, (void *)&params->value1);
+    ck_assert_ptr_ne((void *)&result, (void *)&params->value2);
+  }
+  printf("[PASS] %s\n", params->test_name);
+}
+
+// Основные случаи: разный регистр, цифры, символы
+SUB_TEST_CASES(add_positive, {
+  .value1 = {{0x00000001, 0x00000000, 0x00000000, 0x00000000}}, // 1
+  .value2 = {{0x00000002, 0x00000000, 0x00000000, 0x00000000}}, // 2
+  .expected_result = {{0x00000003, 0x00000000, 0x00000000, 0x00000000}}, // 3
+  .expected_return_code = 0,
+  .test_name = "1 + 2 = 3"
+})
+
+SUB_TEST_CASES(add_negative, {
+  .value1 = {{0x00000005, 0x00000000, 0x00000000, 0x80000000}}, // -5
+  .value2 = {{0x00000003, 0x00000000, 0x00000000, 0x80000000}}, // -3
+  .expected_result = {{0x00000008, 0x00000000, 0x00000000, 0x80000000}}, // -8
+  .expected_return_code = 0,
+  .test_name = "-5 + (-3) = -8"
+})
+
+SUB_TEST_CASES(add_mixed_positive, {
+  .value1 = {{0x0000000A, 0x00000000, 0x00000000, 0x00000000}}, // 10
+  .value2 = {{0x00000004, 0x00000000, 0x00000000, 0x80000000}}, // -4
+  .expected_result = {{0x00000006, 0x00000000, 0x00000000, 0x00000000}}, // 6
+  .expected_return_code = 0,
+  .test_name = "10 + (-4) = 6"
+})
+
+
+SUB_TEST_CASES(add_mixed_negative, {
+  .value1 = {{0x00000005, 0x00000000, 0x00000000, 0x00000000}}, // 5
+  .value2 = {{0x0000000C, 0x00000000, 0x00000000, 0x80000000}}, // -12
+  .expected_result = {{0x00000007, 0x00000000, 0x00000000, 0x80000000}}, // -7
+  .expected_return_code = 0,
+  .test_name = "5 + (-12) = -7"
+})
+
+
+
+Suite *to_lower_suite_create(void) {
+  Suite *s = suite_create("to_lower");
+  TCase *tc = tcase_create("core");
+
+  tcase_add_test(tc, test_add_positive);
+  tcase_add_test(tc, test_add_negative);
+  tcase_add_test(tc, test_add_mixed_negative);
+  tcase_add_test(tc, test_add_mixed_positive);
+  suite_add_tcase(s, tc);
+  return s;
+}
---
+++ src/tests/test_s21_common.h
@@ -0,0 +1,55 @@
+#include "../s21_decimal.h"
+
+#ifndef TEST_COMMON_H
+#define TEST_COMMON_H
+
+typedef struct {
+  const s21_decimal value1;
+  const s21_decimal value2;
+  const s21_decimal expected_result;
+  int expected_return_code; 
+  const char *test_name;
+} addParams;
+
+typedef struct {
+  const char *src;
+  const char *str;
+  const char *expected;
+  const char *test_name;
+} subParams;
+
+typedef struct {
+  const char *src;
+  const char *trim_chars;
+  const char *expected;
+  const char *test_name;
+} mulParams;
+
+typedef struct {
+  const char *str;
+  const char *expected;
+  const char *test_name;
+} divParams;
+
+#define TEST_CASES(name, param_type, run_func, ...)                      \
+  static param_type name[] = {__VA_ARGS__};                              \
+  START_TEST(test_##name) {                                              \
+    for (size_t i = 0; i < sizeof(name) / sizeof(param_type); i++) {     \
+      run_func(&name[i]);                                                \
+    }                                                                    \
+  }                                                                      \
+  END_TEST
+
+#define ADD_TEST_CASES(name, ...) \
+  TEST_CASES(name, addParams, run_add_test, __VA_ARGS__)
+
+#define SUB_TEST_CASES(name, ...) \
+  TEST_CASES(name, subParams, run_sub_test, __VA_ARGS__)
+
+#define MUL_TEST_CASES(name, ...) \
+  TEST_CASES(name, mulParams, run_mul_test)
+
+#define DIV_TEST_CASES(name, ...) \
+  TEST_CASES(name, divParams, run_div_test)
+
+#endif
