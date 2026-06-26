69819651cbaa9db4edfa7fcbcbbcfac5c7ebd51f
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-06-26 10:44:46 +0300

    [brittden:develop-4] - fixed utils header.

--- src/decimal_utils.h
+++ src/decimal_utils.h
@@ -1,20 +1,11 @@
 #include "s21_decimal.h"
-<<<<<<< HEAD
-=======
 #define LAST_BIT 31
 #define SINGLE_BIT 1
->>>>>>> 8df418083bdde2e4cef1d91efe8a4a3dd977a8b8
 
-void align_scales(s21_decimal * value_1,s21_decimal * value_2);
+void align_scales(s21_decimal * value_1, s21_decimal * value_2);
 int get_sign(int service_bits);
 int get_scale(int service_bits);
 int compare_mantissas(s21_decimal value_1, s21_decimal value_2);
-<<<<<<< HEAD
-int will_addition_overflow(s21_decimal value_1, s21_decimal value_2);
-int substract_mantissas(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-void set_sign(s21_decimal *value, int sign);
-void set_scale(s21_decimal *value, int scale);
-=======
 int substract_mantissas(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 void set_sign(s21_decimal *value, int sign);
 void set_scale(s21_decimal *value, int scale);
@@ -22,5 +13,3 @@ int s21_add_mantissas(s21_decimal value_1, s21_decimal value_2, s21_decimal * re
 void shift_left(s21_decimal * value, int bits_count);
 int shift_left_1_bit(s21_decimal * value);
 int mul_by_10(s21_decimal * value);
-void align_scales(s21_decimal * value_1, s21_decimal * value_2);
->>>>>>> 8df418083bdde2e4cef1d91efe8a4a3dd977a8b8
